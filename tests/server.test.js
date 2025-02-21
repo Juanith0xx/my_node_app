4920c3dc12aeb0539c8f6197ef810c2526490a9e
Juan Estay R. <juan.estay.rodriguez@icloud.com>
2025-02-21 15:32:36 -0300

    Add GitHub Actions workflow for testing

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,10 +1,17 @@
-const request = require("supertest");
-const app = require("../server");
+const request = require('supertest');
+const app = require('../server');
 
-describe("GET /", () => {
-  it('should return "Hello World"', async () => {
-    const response = await request(app).get("/");
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe("Hello World");
-  });
+let server;
+
+beforeAll(() => {
+  server = app.listen(9000); // Inicia el servidor antes de las pruebas
+});
+
+afterAll((done) => {
+  server.close(done); // Cierra el servidor después de las pruebas
+});
+
+test('GET / should return "Hello World"', async () => {
+  const response = await request(server).get('/');
+  expect(response.text).toBe('Hello World');
 });
